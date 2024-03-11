5b06394de9a70c2c5b620f568f1e3f7c32906f25
MrAdu8 <125743120+MrAdu8@users.noreply.github.com>
2024-03-11 15:59:39 +0530

    comment removed

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -12,25 +12,4 @@ router.get('/', getallhobbies, globalReponse);
 router.post('/', validateHobbyData, addnewhobby, globalReponse);
 router.put('/:id', validateHobbyData, updatenewhobby, globalReponse);
 
-
-// /* DELETE user listing. */
-// router.delete('/:id', async (req, res, next) => {
-//   try {
-//     const hobbyid = req.params.id;
-
-//     const SQL = "DELETE FROM hobbies WHERE id = ?";
-
-//     const result = await connection.query(SQL, [hobbyid]);
-
-//     if (result.affectedRows === 0) {
-//       res.status(404).json({ err: 'User not found or unable to delete hobby' });
-//     } else {
-//       res.status(200).json({ message: 'User deleted successfully' });
-//     }
-
-//   } catch (error) {
-//     res.status(500).json({ err: 'Unable to delete hobby. Something is wrong!' });
-//   }
-// });
-
 module.exports = router;
